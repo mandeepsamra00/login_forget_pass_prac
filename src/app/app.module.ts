f8e30bddb863b89ac0f3fc4997c0c375825fd3c8
Mandeep Samra <MandeepSamra@compassmade.com>
2023-05-10 08:43:18 -0700

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { ForgetPasswordComponent } from './component/forget-password/forget-password.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
